279783c5f7905a4f8beeb07ee7c50b4ae17ba724
postwo <codo7717@naver.com>
2024-09-25 16:26:10 +0900

    storeuser entity와 repository 등 추가

---
+++ db/src/main/java/com/deliveryservice/db/storeuser/enums/StoreUserStatus.java
@@ -0,0 +1,13 @@
+package com.deliveryservice.db.storeuser.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum StoreUserStatus {
+
+    REGISTERED("등록"),
+    UNREGISTERED("해지"),
+    ;
+
+    private String description;
+}
